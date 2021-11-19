62c4ae3864de97c99f9ab70812ddeb640d2c161f
jiyeon1117 <s2019w24@e-mirim.hs.kr>
2021-11-19 17:22:32 +0900

    complete

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
         <i class="checkBtn fas fa-check" aria-hidden="true"></i>
         {{ todoItem }}
@@ -8,7 +8,7 @@
           <i class="far fa-trash-alt" aria-hidden="true"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </section>
 </template>
 
@@ -48,5 +48,12 @@ export default {
   .removeBtn {
     margin-left: auto;
     color: #de4343;
+  }
+  .list-enter-active, .list-leave-active{
+    transition: all 0.9s;
+  }
+  .list-enter, .list-leave-to{
+    opacity: 0;
+    transform: translateY(15px);
   }
 </style>
